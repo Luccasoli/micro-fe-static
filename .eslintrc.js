569ab80a968233574cd8fd986b08292233f95808
Lucas Mesquita <lucasoliveira98@outlook.com>
2022-01-29 23:28:57 -0300

    feat: migrating to monorepo

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,13 @@
 module.exports = {
-  settings: {
-    react: {
-      version: 'detect',
-    },
-  },
   env: {
     browser: true,
     es2021: true,
+    node: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-    'plugin:react/jsx-runtime',
-    'prettier',
-  ],
+  extends: ['airbnb-base', 'prettier'],
   parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['react'],
   rules: {},
 };
